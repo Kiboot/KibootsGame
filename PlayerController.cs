d07ffc5b06720d4ff81f3b5156ecde8ee0c19753
Kiboot <kevinlee.cardoza@gmail.com>
2022-11-01 15:31:16 +0800

    run, dash and and jump components updated

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,21 +3,61 @@ using System;
 
 public class PlayerController : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+    private int speed = 200;
+    private int gravity = 5000;
+    private float friction = .1f;
+    private float acceleration = 0.5f;
+    private int jumpHeight = 300;
+    private int dashSpeed = 800;
+    private bool isDashing = false;
+    private Vector2 velocity = new Vector2();
     public override void _Ready()
     {
-        
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
  public override void _Process(float delta)
  {
-     if(Input.IsActionJustPressed("ui_left")){};
-     
-     
+    if(!isDashing){
+        int direction = 0;
+
+        if(Input.IsActionPressed("ui_left")){
+            direction -=1;
+        }
+        if(Input.IsActionPressed("ui_right")){
+            direction +=1;
+        }
+        if(direction != 0 ){
+            velocity.x = Mathf.Lerp(velocity.x, direction * speed, acceleration);
+        }else{
+            velocity.x = Mathf.Lerp(velocity.x, 0, friction);
+        }
+    }
+    
+    if (Input.IsActionJustPressed("jump")){
+        if(IsOnFloor()){velocity.y -= 2000;}
+    }
+
+    if(Input.IsActionJustPressed("dash")){
+
+        if(Input.IsActionPressed("ui_left")){
+        velocity.x = -dashSpeed;
+        isDashing = true;
+        }
+        if(Input.IsActionPressed("ui_right")){
+            velocity.x = +dashSpeed;
+            isDashing = true;
+        }
+
+
+    }
+
+
+
+
+    
+
+       velocity.y += gravity * delta;
+       MoveAndSlide(velocity, Vector2.Up);     
  }
 }
